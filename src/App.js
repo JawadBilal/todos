0748a8291f02071d8b87d60f03da64177fc124d2
JawadBilal <jwdbilal447@gmail.com>
2023-11-01 02:35:16 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
+import {Task} from './Task';
+import icon from "../src/images/icon.png";
+import Empty from '../src/images/empty.svg'
 
 function App() {
+  const [list,setList] = useState(()=>{
+    const savedList = localStorage.getItem('list');
+    if(savedList){
+      return JSON.parse(savedList);
+    }
+    else{return [];}
+  });
+  const [newTask, setNewTask] = useState();
+  const [isEmpty,setIsEmpty] = useState(false); 
+   
+  const [editMode,setEditMode] = useState(false);
+  const [updateTask, setUpdateTask] = useState(null);
+  useEffect(()=>{
+    localStorage.setItem('list', JSON.stringify(list));
+  },[list]);
+  useEffect(()=>{ 
+    list.length===0 ? setIsEmpty(true) : setIsEmpty(false);
+  },[]);
+  const inputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+  const addTask = () => {
+    if(!newTask){
+
+    }
+    else{
+      const task={
+        id: list.length===0 ? 1 : list[list.length-1].id + 1,
+        taskName: newTask,
+        completed: false,
+      };
+      setList([...list, task]);
+      setNewTask('');
+      setIsEmpty(false);
+    }
+    
+  };
+  const deleteTask = (id) => {
+    setList(list.filter((task) => task.id !== id));
+    if(list.filter(item => item !== null).length === 0){setIsEmpty(true);}
+  };
+  const editItem = (id) => {
+    let newEditItem = list.find((task)=>{
+      return task.id === id;
+    });
+    console.log(newEditItem);
+    setEditMode(true);
+    setNewTask(newEditItem.taskName);
+    setUpdateTask(id);
+  }; 
+  const updateList = () => {
+    setList(
+      list.map((task)=>{
+        if(task.id === updateTask){
+          return{ ...task, taskName: newTask} 
+        }
+        return task;
+      })
+    );
+    setEditMode(false);  
+    setNewTask('');
+    setUpdateTask(null);
+  }
+  const doneTask = (id) => {
+    setList(list.map(task => task.id === id ? {...task, completed:!task.completed} : task));
+  };
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+        <h2>To-Do List <img src={icon} alt="icon"/></h2>
+        <div className='addTask'>
+          <input type='text' onChange={inputChange} value={newTask} placeholder='Add Task'/>
+          {
+            editMode ? <button onClick={updateList}>Update</button> : <button onClick={addTask}>Add</button>
+          }
+        
+        </div>
+        <div className='list'>
+          {
+          isEmpty ? 
+          <div className='emptyList'>
+            <img src={Empty}/>
+            <h3>Empty...</h3>
+          </div>
+          :
+          list.map((task) => {
+            return (
+            <div>
+              <Task 
+              taskName = {task.taskName}
+              id = {task.id}
+              completed = {task.completed}
+              deleteTask = {deleteTask}
+              editItem = {editItem}
+              doneTask = {doneTask}
+              />
+              
+              
+            
+              
+              
+            </div>)
+          })
+          }
+        </div>
+
+      </div>
+      
+        
     </div>
   );
 }
---
+++ src/Task.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import deleteIcon from '../src/images/delete1.svg';
+import editIcon from './images/edit1.svg';
+
+
+export const Task = (props) => {
+    return(
+        <div className={`taskList ${props.completed ? 'done' : ''}`}>
+            <div onClick={()=>props.doneTask(props.id)} className={props.completed ? 'doneTask' : ''}>
+                 {props.taskName} </div>
+            <div className="functionBtn">
+                <button disabled={props.completed ? true:false} onClick={()=>props.editItem(props.id)}><img src={editIcon} className={props.completed ? "":"editIcon"} /></button>
+                <button onClick={() => props.deleteTask(props.id)}><img src={deleteIcon} alt="close button" className="deletebtn"/></button>
+            </div>
+            
+        </div>
+    );
+};
